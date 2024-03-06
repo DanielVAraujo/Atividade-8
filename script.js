cf0c59d50c157f2fdca40582a8af616fc8cd1427
DanielVAraujo <160507946+DanielVAraujo@users.noreply.github.com>
2024-03-06 12:12:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function soma(){
+                let num1; 
+                let num2;
+                let resultado;
+                num1=3;
+                num2=4;
+                resultado=num1+num2;
+                document.getElementById("JSScript").innerHTML=resultado;
+
+}
+
+function sortear(){
+                    let aleat;
+                    aleat=Math.floor(Math.random()*11);
+                    document.getElementById("sorteia").innerHTML=aleat
+
+}
